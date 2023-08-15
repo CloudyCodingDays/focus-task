1efbaea1682ccfc9750b55e85358937430da76e6
PLyn <plyn8750@gmail.com>
2023-08-14 21:13:43 -0500

    Adjust filter widths

--- components/SearchFilters.tsx
+++ components/SearchFilters.tsx
@@ -1,9 +1,9 @@
 const SearchFilters = () => {
   return (
     <div>
-      <div className="flex flex-row justify-around items-baseline lg:w-[1000px] lg:mx-auto w-full mb-4 text-sm">
+      <div className="flex flex-row justify-around items-baseline lg:w-[1000px] mx-auto w-11/12 mb-4 text-sm">
         Filters:
-        <button className="hover:bg-green-500 hover:text-gray-100 bg-white text-gray-500 border-2 border-green-500 px-2 py-2 rounded-lg">
+        <button className="hover:bg-green-500 hover:text-gray-100 bg-white text-gray-500 border-2 border-green-500 px-2 rounded-lg">
           Show All
         </button>
         <button className="hover:bg-green-500 hover:text-gray-100 bg-green-300 text-gray-500 px-2 py-2 rounded-lg">
@@ -12,7 +12,7 @@ const SearchFilters = () => {
         <button className="hover:bg-yellow-500 hover:text-gray-100 bg-yellow-300 text-gray-500 px-2 py-2 rounded-lg">
           I can manage a little more
         </button>
-        <button className="hover:bg-purple-500 hover:text-gray-100 bg-purple-300 text-gray-500 rounded-lg">
+        <button className="hover:bg-purple-500 hover:text-gray-100 bg-purple-300 text-gray-500 px-2 py-2 rounded-lg">
           I got a burst of energy to get stuff done
         </button>
       </div>
