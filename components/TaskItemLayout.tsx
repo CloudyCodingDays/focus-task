a35ffe34af6ebf826111f4e6c7a94de1595928e6
PLyn <plyn8750@gmail.com>
2023-08-17 14:59:58 -0500

    Fix padding on mobile

--- components/TaskItemLayout.tsx
+++ components/TaskItemLayout.tsx
@@ -64,7 +64,7 @@ const TaskItemLayout: React.FC<TaskItemProps> = ({ task }) => {
 
   return (
     <div key={id} className="rounded-lg flex flex-row">
-      <div className="flex flex-col px-4 py-2 w-full">
+      <div className="flex flex-col px-2 py-2 w-full">
         <div className="flex flex-row justify-between">
           <div>
             <div className="flex flex-row text-sm text-start font-semibold">
@@ -81,12 +81,12 @@ const TaskItemLayout: React.FC<TaskItemProps> = ({ task }) => {
             </div>
           </div>
           <div>
-            <div className="flex flex-row items-center text-xs px-4">
+            <div className="flex flex-row items-center text-xs">
               <AlertCircle size={20} color={priorityIconStyle} />
               <div className="pl-2">{priority} Priority</div>
             </div>
             {task.is_recurring ? (
-              <div className="flex flex-row items-center text-xs font-light px-4 pt-4">
+              <div className="flex flex-row items-center text-xs font-light pt-4">
                 <Repeat />
                 <div className="pl-2">Repeats {task.recurring_type}</div>
               </div>
