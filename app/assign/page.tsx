55b1aedc8a6d2a2d7f7102e410705e176af917c5
PLyn <plyn8750@gmail.com>
2023-09-05 16:16:04 -0500

    Dynamic imports for assign and manage

--- app/assign/page.tsx
+++ app/assign/page.tsx
@@ -1,6 +1,13 @@
-import AssignTaskListLayout from "./components/AssignTaskListLayout";
+import dynamic from "next/dynamic";
+import { Skeleton } from "@/components/ui_components/skeleton";
 
 export default function Home() {
+  const AssignTaskListLayout = dynamic(
+    () => import("./components/AssignTaskListLayout"),
+    {
+      loading: () => <Skeleton className="mx-auto w-[400px] h-[30px] mt-4" />,
+    },
+  );
   return (
     <div>
       <AssignTaskListLayout />
